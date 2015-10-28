154c815b0f1b0e7155951e7e8785c99fd426afb2
Alboukadel Kassambara <alboukadel.kassambara@gmail.com>
2015-10-28 02:34:47 +0100

    Example section modified

--- R/hkmeans.R
+++ R/hkmeans.R
@@ -36,14 +36,22 @@
 #' data(USArrests)
 #' # Scale the data
 #' df <- scale(USArrests)
+#' 
 #' # Compute hierarchical k-means clustering
 #' res.hk <-hkmeans(df, 4)
+#' 
 #' # Elements returned by hkmeans()
 #' names(res.hk)
+#' 
 #' # Print the results
 #' res.hk
+#' 
 #' # Visualize the tree
 #' hkmeans_tree(res.hk, cex = 0.6)
+#' # or use this
+#' fviz_dend(res.hk, cex = 0.6)
+#' 
+#' 
 #' # Visualize the hkmeans final clusters
 #' fviz_cluster(res.hk, frame.type = "norm", frame.level = 0.68)
 #' }
