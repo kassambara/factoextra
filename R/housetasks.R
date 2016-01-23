90d04c49a203d1a51b48322809d22f6282b449ac
Alboukadel Kassambara <alboukadel.kassambara@gmail.com>
2016-01-23 15:45:11 +0100

    firsr commit

---
+++ R/housetasks.R
@@ -0,0 +1,26 @@
+#'House tasks contingency table
+#'
+#'@description A data frame containing the frequency of execution of 13 house
+#'  tasks in the couple. This table is also available in ade4 package.
+#'@name housetasks
+#'@docType data
+#'@usage data("housetasks")
+#'@format  
+#' A data frame with 13 observations (house tasks) on the following 4 columns.
+#' \describe{
+#'  \item{\code{Wife}}{a numeric vector}
+#'  \item{\code{Alternating}}{a numeric vector}
+#'  \item{\code{Husband}}{a numeric vector}
+#'  \item{\code{Jointly}}{a numeric vector}
+#' }
+#'@source This data is from FactoMineR package.
+#'
+#' @examples
+#' \donttest{
+#' library(FactoMineR)
+#' data(poison)
+#' res.mca <- MCA(poison, quanti.sup = 1:2, quali.sup=c(3,4), graph = FALSE)
+#' fviz_mca_biplot(res.mca)
+#' }
+#' 
+NULL
