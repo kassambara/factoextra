793c7f385d8434df7232f54fdbc016583b5caa5e
Alboukadel Kassambara <alboukadel.kassambara@gmail.com>
2016-04-26 11:54:53 +0200

    example section updated

--- R/poison.R
+++ R/poison.R
@@ -14,8 +14,11 @@
 #' \donttest{
 #' library(FactoMineR)
 #' data(poison)
-#' res.mca <- MCA(poison, quanti.sup = 1:2, quali.sup=c(3,4), graph = FALSE)
-#' fviz_mca_biplot(res.mca)
+#' res.mca <- MCA(poison, quanti.sup = 1:2, quali.sup = c(3,4), 
+#'    graph = FALSE)
+#' fviz_mca_biplot(res.mca, repel = TRUE)+
+#' theme_minimal()
+#' 
 #' }
 #' 
 NULL
