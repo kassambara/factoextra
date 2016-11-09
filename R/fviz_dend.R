c38461dc6dad56bf16f608fcb79712613af97ace
Alboukadel Kassambara <alboukadel.kassambara@gmail.com>
2016-11-09 10:51:54 +0100

    phylogenic tree supported now

--- R/fviz_dend.R
+++ R/fviz_dend.R
@@ -1,12 +1,12 @@
 #' Enhanced Visualization of Dendrogram
 #' 
-#' @description Draws easily beautiful dendrogram using either R base plot or 
-#'   ggplot2. Provides also an option for drawing a circular dendrogram.
+#' @description Draws easily beautiful dendrograms using either R base plot or 
+#'   ggplot2. Provides also an option for drawing a circular dendrogram and phylogenic trees.
 #' @param x an object of class dendrogram, hclust, agnes, diana, hcut or 
 #'   hkmeans.
 #' @param k the number of groups for cutting the tree.
 #' @param k_colors a vector containing colors to be used for the groups. It 
-#'   should contains k number of colors. If as.ggplot = TRUE, Allowed values 
+#'   should contains k number of colors. If as.ggplot = TRUE or type = "phylogenic", Allowed values 
 #'   include also "grey" for grey color palettes; brewer palettes e.g. "RdBu", 
 #'   "Blues", ...;  and scientific journal palettes from ggsci R package, e.g.: 
 #'   "npg", "aaas", "lancet", "jco", "ucscgb", "uchicago", "simpsons" and 
@@ -18,8 +18,9 @@
 #' @param label_cols a vector containing the colors for labels.
 #' @param labels_track_height a numeric value for adjusting the room for the
 #'   labels. Ignored when as.ggplot = FALSE.
+#' @param repel logical value. Use repel = TRUE to avoid label overplotting when type = "phylogenic".
 #' @param lwd a numeric value specifying branches line width.
-#' @param type type of plot. Allowed values are one of "rectangle" or "triangle"
+#' @param type type of plot. Allowed values are one of "rectangle", "triangle", "circular", "phylogenic".
 #' @param rect logical value specifying whether to add a rectangle around 
 #'   groups. Used only when k != NULL.
 #' @param rect_border,rect_lty,rect_lwd border color, line type and line width 
@@ -27,7 +28,6 @@
 #' @param as.ggplot a logical value. If TRUE, the dendrogram is plotted using 
 #'   ggplot2.
 #' @param horiz a logical value. If TRUE, an horizontal dendrogram is drawn.
-#' @param circular a logical value. If TRUE, draws a circular dendogram.
 #' @param cex size of labels
 #' @param main,xlab,ylab main and axis titles
 #' @param sub Plot subtitle. If NULL, the method used hierarchical clustering is
@@ -78,10 +78,10 @@
 #' }
 #' @export
 fviz_dend <- function(x, k = NULL, k_colors = NULL,  show_labels = TRUE, color_labels_by_k = TRUE,
-                      label_cols = NULL,  labels_track_height = 1, lwd = 1,
-                      type = c("rectangle", "triangle"),
+                      label_cols = NULL,  labels_track_height = 1, repel = FALSE, lwd = 1,
+                      type = c("rectangle", "triangle", "circular", "phylogenic"),
                       rect = FALSE, rect_border = "gray", rect_lty = 2, rect_lwd = 1.5, 
-                      as.ggplot = FALSE, ggtheme = theme_classic(), horiz = FALSE, circular = FALSE,
+                      as.ggplot = FALSE, ggtheme = theme_classic(), horiz = FALSE, 
                       cex = 0.8, main = "Cluster Dendrogram", xlab = "", ylab = "Height", 
                       sub = NULL, ...)
 {
@@ -89,6 +89,10 @@ fviz_dend <- function(x, k = NULL, k_colors = NULL,  show_labels = TRUE, color_l
   if(.is_col_palette(k_colors)) palette <- k_colors
   else palette <- NULL
   if(!color_labels_by_k & is.null(label_cols)) label_cols <- "black"
+  type <- match.arg(type)
+  circular <- type == "circular"
+  phylogenic <- type == "phylogenic"
+  
   
   if(inherits(x, "hcut")){
     k <- x$nbclust
@@ -109,9 +113,12 @@ fviz_dend <- function(x, k = NULL, k_colors = NULL,  show_labels = TRUE, color_l
     method <- ""
   }
   else stop("Can't handle an object of class ", paste(class(x), collapse =", ") )
+  if(is.na(method)) method <- ""
   if(is.null(sub) & method!="") sub = paste0("Method: ", method)
   if(as.ggplot |circular) lwd <- lwd/1.5
   
+  
+  if(!is.null(dendextend::labels_cex(dend))) cex <- dendextend::labels_cex(dend)
   dend <- dendextend::set(dend, "labels_cex", cex) 
   dend <- dendextend::set(dend, "branches_lwd", lwd) 
   
@@ -135,9 +142,15 @@ fviz_dend <- function(x, k = NULL, k_colors = NULL,  show_labels = TRUE, color_l
   if(as.ggplot | circular){
     p <- .ggplot_dend(dend, type = "rectangle", offset_labels = -0.1, nodes = FALSE,
                       ggtheme = ggtheme, horiz = horiz, circular = circular, palette = palette,
-                      label_cols = label_cols, labels_track_height = labels_track_height, ...)
+                      labels = show_labels, label_cols = label_cols, 
+                      labels_track_height = labels_track_height, ...)
     if(!circular) p <- p + labs(title = main, x = xlab, y = ylab)
     return(p)
+  }
+  else if(phylogenic){
+    .phylogenic_tree(dend, labels = show_labels, label_cols = label_cols,
+                                 palette = palette, repel = repel,
+                                 ggtheme = ggtheme, ...)
   }
   else{
     plot(dend,  type = type[1], xlab = xlab, ylab = ylab, main = main,
@@ -149,12 +162,83 @@ fviz_dend <- function(x, k = NULL, k_colors = NULL,  show_labels = TRUE, color_l
 }
 
 
+# require igraph
+.phylogenic_tree <- function(dend, labels = TRUE, label_cols = NULL,
+                              palette = NULL, repel = FALSE,
+                              ggtheme = theme_classic(), ...){
+  
+  if (!requireNamespace("igraph", quietly = TRUE)) {
+    stop("igraph package needed for phylogenic tree. Please install it using install.packages('igraph').")
+  }
+  
+  # Convert to 'phylo' object
+  hc <- stats::as.hclust(dend)
+  phylo_tree <- .as.phylo(hc)
+  graph_edges <- phylo_tree$edge
+  
+  # get graph from edge list
+  graph_net <- igraph::graph.edgelist(graph_edges)
+  
+  # extract layout (x-y coords)
+  set.seed(123)
+  graph_layout = igraph::layout.auto(graph_net)
+  # number of observations
+  nobs <- length(hc$labels)
+  
+  # draw tree branches
+  data.segments <- data.frame(
+    x = graph_layout[graph_edges[,1],1], 
+    y = graph_layout[graph_edges[,1],2],
+    xend = graph_layout[graph_edges[,2],1],
+    yend = graph_layout[graph_edges[,2],2]
+  )
+  
+  data.labels <- data.frame(
+    x = graph_layout[1:nobs,1], 
+    y = graph_layout[1:nobs,2],
+    label = phylo_tree$tip.label
+  )
+  data.labels <- data.labels[order(as.vector(data.labels$label)), ]
+  
+  # Support for dendextend
+  gdend <- dendextend::as.ggdend(dend)
+  gdat <- dendextend::prepare.ggdend(gdend)
+  gdat$labels <- gdat$labels[order(as.vector(gdat$labels$label)), ]
+  data.labels <- cbind(data.labels, gdat$labels[, c("col", "cex")])
+  if(!is.null(dendextend::labels_cex(dend))) font.label <- round(dendextend::labels_cex(dend)[1]*12)
+  else font.label <- 12
+  
+  p <- ggplot() + geom_segment(data = data.segments,
+                    aes_string(x = "x", y = "y", xend = "xend", yend = "yend"),
+                    lineend = "square")
+  if(is.null(label_cols)) label_cols <- "col"
+  if(!labels) labels <- NULL
+  else labels <- "label"
+  
+  p <- ggpubr::ggscatter(data.labels, "x", "y", label = labels,
+                         color = label_cols,
+                         ggp = p, repel = repel, font.label = font.label, ...)
+  
+  if(is.null(palette)) p <- p + scale_colour_identity()
+  
+  p <- ggpubr::ggpar(p, ggtheme = ggtheme, palette = palette, ...) 
+  
+  p <- p + theme(axis.title.x = element_blank(), axis.title.y = element_blank(),
+                 axis.text= element_blank(), 
+                 axis.line = element_blank(), axis.ticks = element_blank(),
+                 legend.position = "none")
+  p
+}
+
+
 # Helper functions
 #%%%%%%%%%%%%%%%%%%%%
+
+# Plot dendrogram using ggplot
 # .ggplot_dend derrived from dendextend::ggplot.ggdend
 # data: a ggdend class object.
 .ggplot_dend <- function (dend, segments = TRUE, labels = TRUE, nodes = TRUE, 
-                        horiz = FALSE, ggtheme = theme_ggdend(), 
+                        horiz = FALSE, ggtheme = theme_classic(), 
                         offset_labels = 0, circular = FALSE, type = "rectangle",
                         palette = NULL, label_cols = NULL, labels_track_height = 1,
                         ...) {
@@ -230,6 +314,7 @@ fviz_dend <- function(x, k = NULL, k_colors = NULL,  show_labels = TRUE, color_l
   p
 }
 
+# Function used for circular dendrogram
 # Create the angle and hjust vectors so that the labels 
 # rotation switches from 6 o'clock to 12 o'clock to improve readability. 
 .get_label_params <- function(labeldf) {        
@@ -249,6 +334,7 @@ fviz_dend <- function(x, k = NULL, k_colors = NULL,  show_labels = TRUE, color_l
 }
 
 
+# Check if color palette
 .is_col_palette <- function(palette){
   brewerpal <- c(
     # sequential
@@ -267,6 +353,45 @@ fviz_dend <- function(x, k = NULL, k_colors = NULL,  show_labels = TRUE, color_l
   
   if(is.null(palette)) return(FALSE)
   else return(length(palette)==1 & palette[1] %in% c(brewerpal, ggscipal))
+}
+
+
+# Convert 'hclust' to 'phylo' object
+# from ape::as.phylo.hclust
+# x hclust
+.as.phylo <- function (x, ...) 
+{
+  N <- dim(x$merge)[1]
+  edge <- matrix(0L, 2 * N, 2)
+  edge.length <- numeric(2 * N)
+  node <- integer(N)
+  node[N] <- N + 2L
+  cur.nod <- N + 3L
+  j <- 1L
+  for (i in N:1) {
+    edge[j:(j + 1), 1] <- node[i]
+    for (l in 1:2) {
+      k <- j + l - 1L
+      y <- x$merge[i, l]
+      if (y > 0) {
+        edge[k, 2] <- node[y] <- cur.nod
+        cur.nod <- cur.nod + 1L
+        edge.length[k] <- x$height[i] - x$height[y]
+      }
+      else {
+        edge[k, 2] <- -y
+        edge.length[k] <- x$height[i]
+      }
+    }
+    j <- j + 2L
+  }
+  if (is.null(x$labels)) 
+    x$labels <- as.character(1:(N + 1))
+  obj <- list(edge = edge, edge.length = edge.length/2, tip.label = x$labels, 
+              Nnode = N)
+  class(obj) <- "phylo"
+  obj
+  #ape::reorder.phylo(obj)
 }
 
 
