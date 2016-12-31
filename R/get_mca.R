50cf571018fe057263295791fa0b874fabd4f258
kassambara <alboukadel.kassambara@gmail.com>
2016-12-31 18:34:10 +0100

    updated

--- R/get_mca.R
+++ R/get_mca.R
@@ -2,22 +2,22 @@
 NULL
 #' Extract the results for individuals/variables - MCA
 #' 
-#' @description
-#' Extract all the results (coordinates, squared cosine and contributions) 
-#' for the active individuals/variable categories from Multiple Correspondence Analysis (MCA) outputs.\cr\cr
-#' \itemize{
-#' \item get_mca(): Extract the results for variables and individuals
-#' \item get_mca_ind(): Extract the results for individuals only
-#' \item get_mca_var(): Extract the results for variables only
-#' }
+#' @description Extract all the results (coordinates, squared cosine and 
+#'   contributions) for the active individuals/variable categories from Multiple
+#'   Correspondence Analysis (MCA) outputs.\cr\cr \itemize{ \item get_mca(): 
+#'   Extract the results for variables and individuals \item get_mca_ind(): 
+#'   Extract the results for individuals only \item get_mca_var(): Extract the 
+#'   results for variables only }
 #' @param res.mca an object of class MCA [FactoMineR], acm [ade4].
-#' @param element the element to subset from the output. Possible values are "var" or "ind".
+#' @param element the element to subset from the output. Possible values are 
+#'   "var" for variables, "ind" for individuals, "mca.cor" for correlation
+#'   between variables and principal dimensions, "quanti.sup" for quantitative supplementary variables.
 #' @return a list of matrices containing the results for the active 
-#' individuals/variable categories including : 
-#' \item{coord}{coordinates for the individuals/variable categories}
-#' \item{cos2}{cos2 for the individuals/variable categories}
-#' \item{contrib}{contributions of the individuals/variable categories}
-#' \item{inertia}{inertia of the individuals/variable categories}
+#'   individuals/variable categories including : \item{coord}{coordinates for 
+#'   the individuals/variable categories} \item{cos2}{cos2 for the 
+#'   individuals/variable categories} \item{contrib}{contributions of the 
+#'   individuals/variable categories} \item{inertia}{inertia of the 
+#'   individuals/variable categories}
 #' @author Alboukadel Kassambara \email{alboukadel.kassambara@@gmail.com}
 #' @references http://www.sthda.com/english/
 #' @examples
@@ -52,37 +52,58 @@ NULL
 #'  }
 #'  
 #' @name get_mca
-#' 
+#'   
 #' @rdname get_mca
-#' @export 
-get_mca <- function(res.mca, element = c("var", "ind")){
- elmt <- element[1]
- if(elmt =="var") get_mca_var(res.mca)
- else if(elmt == "ind") get_mca_ind(res.mca)
- else stop("Allowed values for the argument element are: 'var' or 'ind'.")
+#' @export
+get_mca <- function(res.mca, element = c("var", "ind", "mca.cor", "quanti.sup")){
+ elmt <- match.arg(element)
+ if(elmt == "ind") get_mca_ind(res.mca)
+ else get_mca_var(res.mca, elmt)
 }
 
 
 #' @rdname get_mca
 #' @export
-get_mca_var <- function(res.mca){
+get_mca_var <- function(res.mca, element = c( "var", "mca.cor", "quanti.sup")){
+  choice <- match.arg(element)
   # FactoMineR package
-  if(inherits(res.mca, c("MCA"))) vars <- res.mca$var
+  if(inherits(res.mca, c("MCA"))) {
+    vars <- switch(choice,
+                   var = res.mca$var,
+                   mca.cor = list(coord = res.mca$var$eta2),
+                   quanti.sup = res.mca$quanti.sup
+    )
+    element_desc <- switch(choice,
+                           var = "variables",
+                           mca.cor = "correlation between variables and dimensions",
+                           quanti.sup = "quantitative supplementary variables"
+    )
+  }
   # ade4 package
   else if(inherits(res.mca, "acm") & inherits(res.mca, 'dudi')){
     if (!requireNamespace("ade4", quietly = TRUE)) {
       stop("ade4 package needed for this function to work. Please install it.")
     }
+    element_desc = "variables"
+    if(choice == "var"){
     coord <- res.mca$co
     inertia <- ade4::inertia.dudi(res.mca, row.inertia = FALSE, col.inertia = TRUE)
     cos2 <- abs(inertia$col.rel/10000)[, colnames(coord)]
     contrib <- (inertia$col.abs/100)[, colnames(coord)]
     colnames(coord) <- colnames(cos2) <- colnames(contrib) <- paste0("Dim.", 1:ncol(coord)) 
     vars <- list(coord = coord, contrib = contrib, cos2 = cos2)
+    }
+    else{
+      vars <- list()
+      stop("Don't handle ", choice, "for MCA computed with ade4. Use FactoMineR instead.")
+    }
   }
   else stop("An object of class : ", class(res.mca), 
             " can't be handled by the function get_mca_var()")
-  class(vars)<-c("factoextra", "mca_var")
+  element_class <-switch(choice,
+                         var = "mca_var", mca.cor = "mca_cor", quanti.sup = "quanti.sup")
+  class(vars)<-c("factoextra",  "mca", element_class)
+  attr(vars, "element") <- element_desc
   return(vars)
 }
 
@@ -106,7 +127,8 @@ get_mca_ind <- function(res.mca){
   
   else stop("An object of class : ", class(res.mca), 
             " can't be handled by the function get_mca_ind()")
-  class(ind)<-c("factoextra", "mca_ind")
+  class(ind)<-c("factoextra", "mca", "mca_ind")
+  attr(ind, "element") <- "individuals"
   return(ind)
 }
 
