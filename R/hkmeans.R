22741a1a6187a8ee79be7af443261b37e200b152
Alboukadel Kassambara <alboukadel.kassambara@gmail.com>
2017-12-15 20:26:21 +0100

    fix for #52

--- R/hkmeans.R
+++ R/hkmeans.R
@@ -66,7 +66,7 @@
 hkmeans <- function(x, k, hc.metric = "euclidean", hc.method = "ward.D2",
                     iter.max = 10, km.algorithm = "Hartigan-Wong"){
   
-  res.hc <- stats::hclust(stats::dist(x, method = "euclidean"), method = hc.method)
+  res.hc <- stats::hclust(stats::dist(x, method = hc.metric), method = hc.method)
   grp <- stats::cutree(res.hc, k = k)
   clus.centers <- stats::aggregate(x, list(grp), mean)[, -1]
   res.km <- kmeans(x, centers = clus.centers, 
